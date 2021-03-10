c806151100326f03cc9ef7a9f84170e1ec2506d9
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-03-10 15:16:46 +0000

    Adding on click events

---
+++ assets/js/onclick.js
@@ -0,0 +1,30 @@
+var button = $( "#collapser" );
+var tabimg = $( "#icon-img" );
+var stp = $( "#savethepub" );
+
+var bntclicked = false;
+
+button.click(function() {
+    if (bntclicked = false) {
+        tabimg.css("max-width", "70px");
+        tabimg.css("top", "8%!important");
+        tabimg.css("left", "21%!important");
+        tabimg.css("z-index", "0");
+
+        stp.css("top", "7%!important");
+        stp.css("font-size", "22px");
+
+        bntclicked = true;
+    }
+    if (bntclicked = true) {
+        tabimg.css("max-width", "113px");
+        tabimg.css("top", "43%!important");
+        tabimg.css("left", "");
+        tabimg.css("z-index", "0");
+
+        stp.css("top", "84%!important");
+        stp.css("font-size", "22px");
+
+        bntclicked = false;
+    }
+});
