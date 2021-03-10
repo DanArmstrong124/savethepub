818905358f36ec388dc42c4992d68237387e5433
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-03-10 16:13:29 +0000

    Removing tests

--- assets/js/onclick.js
+++ assets/js/onclick.js
@@ -6,7 +6,6 @@ var bntclicked = false;
 
 button.click(function() {
     if (bntclicked === false) {
-        alert("FALSE");
         $( "#icon-img" ).css("max-width", "70px");
         tabimg.css("top", "8%!important");
         tabimg.css("left", "21%!important");
@@ -18,7 +17,6 @@ button.click(function() {
         bntclicked = true;
     }
     else if (bntclicked === true) {
-        alert("TRUE");
         $( "#icon-img" ).css("max-width", "113px");
         tabimg.css("top", "43%!important");
         tabimg.css("left", "");
